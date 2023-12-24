4c733a12029a5f95c9bb63e4ddadd1cb10b6d270
Rohith1510 <rohithpranav567@gmail.com>
2023-12-24 22:50:00 +0530

    Second Commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+// Import and configure dotenv
+require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-      console.log(account.address);
-  }
-});
-
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      // This value will be replaced on runtime
+      url: process.env.ALCHEMY_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+
+    },
+   
+  },
 };
